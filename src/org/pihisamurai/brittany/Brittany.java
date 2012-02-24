67184b9d609338f6b8d42735acdd5f820695886a
Michael Huang <mike.g.huang@gmail.com>
2012-02-24 18:21:17 -0500

    First commit based on IterativeRobot

---
+++ src/org/pihisamurai/brittany/Brittany.java
@@ -0,0 +1,20 @@
+package org.pihisamurai.brittany;
+
+
+import edu.wpi.first.wpilibj.IterativeRobot;
+
+public class Brittany extends IterativeRobot {
+
+    public void robotInit() {
+
+    }
+
+    public void autonomousPeriodic() {
+
+    }
+
+    public void teleopPeriodic() {
+        
+    }
+    
+}
