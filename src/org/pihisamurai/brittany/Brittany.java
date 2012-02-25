8d2c81926782011a7c2d42b0fa021c3cb1aada9e
Daniel Bole <danielbole@comcast.net>
2012-02-24 19:46:51 -0500

    Work on main robot class

--- src/org/pihisamurai/brittany/Brittany.java
+++ src/org/pihisamurai/brittany/Brittany.java
@@ -2,11 +2,19 @@ package org.pihisamurai.brittany;
 
 
 import edu.wpi.first.wpilibj.IterativeRobot;
+import edu.wpi.first.wpilibj.DriverStation;
 
 public class Brittany extends IterativeRobot {
 
+    DriverStation driverStation;
+    DriverStationBuffer buffer;
+
     public void robotInit() {
 
+        buffer = new DriverStationBuffer();
+        driverStation = DriverStation.getInstance();
+
+        buffer.println("*** Robot Init ***");
     }
 
     public void autonomousPeriodic() {
