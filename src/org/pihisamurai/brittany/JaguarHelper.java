712de8841f687423a8367a43e82a1022a8701f06
Daniel Bole <danielbole@comcast.net>
2012-02-24 19:19:45 -0500

    Added JaguarHelper

---
+++ src/org/pihisamurai/brittany/JaguarHelper.java
@@ -0,0 +1,25 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package org.pihisamurai.brittany;
+
+import edu.wpi.first.wpilibj.Jaguar;
+import edu.wpi.first.wpilibj.DigitalOutput;
+
+/**
+ *
+ * @author Daniel
+ */
+public class JaguarHelper {
+
+    Jaguar jaguar;
+    DigitalOutput digitalOutput;
+
+    JaguarHelper(int SLOT, int PLUG) {
+        jaguar = new Jaguar(SLOT, PLUG);
+        digitalOutput = new DigitalOutput(SLOT, PLUG);
+        digitalOutput.disablePWM();
+    }
+}
